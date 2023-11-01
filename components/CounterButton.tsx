9196a7eb085c7a9d239c146004c3d3f3c488a146
anusheel <anusheel@gmail.com>
2023-11-01 10:24:28 +0000

    Commit from Stub AI

---
+++ components/CounterButton.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+const CounterButton = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-white flex items-center justify-center h-screen">
+      <button
+        className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
+        onClick={() => setCount(count + 1)}
+      >
+        count = {count}
+      </button>
+    </div>
+  )
+}
+
+export default CounterButton
